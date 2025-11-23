fd9ad4eee3b0880f15ea939fe497ebd442fb84b8
WeihangGuo <weihang.guo2001@gmail.com>
2025-11-22 23:10:24 -0600

    Simplify conversion from OMPL state to JAX array.

--- src/omplx/utils/conversion.py
+++ src/omplx/utils/conversion.py
@@ -1,4 +1,5 @@
 import jax.numpy as jnp
 def state_to_jax_array(state, dim):
     """Convert OMPL state to JAX array."""
-    return jnp.array([state[i] for i in range(dim)])
+    # return jnp.array([state[i] for i in range(dim)])
+    return jnp.array(state[:dim])
